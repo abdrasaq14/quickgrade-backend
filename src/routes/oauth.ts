6e9f962e57b5fc32018ec2b651b343bbbbcb3565
samuel fagbohun <samuelfagbohun041@gmail.com>
2024-01-17 00:12:12 +0100

    retrieving userData correctly

--- src/routes/oauth.ts
+++ src/routes/oauth.ts
@@ -8,38 +8,15 @@ const router = express.Router();
 async function getUserData(accessToken: any): Promise<any> {
   try {
     const response = await fetch(
-      `https://googleapis.com/oauth2/v3/userinfo?accessToken=${accessToken}`,
+      `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${accessToken}`,
     );
     const data = await response.json();
     console.log('data: ', data);
-    // return data;
+    return data;
   } catch (error) {
     console.log('Error with getting user data');
   }
 }
-
-/* GET home page. */
-// router.get('/', async function (req, res, next): Promise<void>{
-//   const code: any = req.query.code;
-//   try {
-//     const redirectUrl = 'http://127.0.0.1:3000/oauth';
-
-//     const oAuth2Client = new OAuth2Client(
-//       process.env.CLIENT_ID,
-//       process.env.CLIENT_SECRET,
-//       redirectUrl,
-//     );
-//     const response = await oAuth2Client.getToken(code);
-//     oAuth2Client.setCredentials(response.tokens);
-//     console.log('Tokens acquired.');
-//     const user = oAuth2Client.credentials;
-//     console.log('creadentials: ', user);
-//     await getUserData(user.access_token);
-
-//   } catch (error) {
-//     console.log('Error with signing in with Google');
-//   }
-// });
 
 router.get('/', function (req, res, next): void {
   const code: any = req.query.code;
@@ -59,14 +36,12 @@ router.get('/', function (req, res, next): void {
         console.log('Tokens acquired.');
         const user = oAuth2Client.credentials;
         console.log('creadentials: ', user);
-        return await getUserData(user.access_token);
-      })
-      .then(() => {
+        const userData = await getUserData(user.access_token);
         // Handle userData
-        // For example, you can send a response using res.json({ userData });
+        res.json({ userData });
+        console.log('userData: ', userData);
       })
       .catch((error) => {
-        //   console.log('Error with signing in with Google');
         console.log(error);
       });
   } catch (error) {
