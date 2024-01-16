eebe846fd1af8ad791e72dea03b2ea31bf1b5acf
samuel fagbohun <samuelfagbohun041@gmail.com>
2024-01-16 18:33:21 +0100

    google-sign-in

--- src/app.ts
+++ src/app.ts
@@ -10,9 +10,12 @@ import cookieParser from 'cookie-parser';
 import sequelize from './database/databaseSqlite';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
+import oauthRouter from './routes/oauth';
+import requestRouter from './routes/request';
 
 config();
 
@@ -26,6 +29,13 @@ sequelize
   });
 const app = express();
 
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }),
+);
+
 // view engine setup
 app.set('views', path.join(__dirname, '../', 'views'));
 app.set('view engine', 'ejs');
@@ -38,6 +48,8 @@ app.use(express.static(path.join(__dirname, '../', 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/oauth', oauthRouter);
+app.use('/request', requestRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
