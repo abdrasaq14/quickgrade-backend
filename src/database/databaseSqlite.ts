02aebe8c723136379e22f682d40cf47bb879d76d
abdrasaq14 <abdrasaq14@gmail.com>
2024-03-17 11:47:25 +0100

    modified the databasconfig file

--- src/database/databaseSqlite.ts
+++ src/database/databaseSqlite.ts
@@ -1,9 +1,13 @@
 import { Sequelize } from 'sequelize'
+import { config } from 'dotenv'
 
+config()
+const BACKEND_URL = process.env.BACKEND_URL
+console.log('BACKEND_URL:', BACKEND_URL)
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: './database.sqlite',
-  host: 'https://quickgrade-backend-kg24.onrender.com/', // Update this to the URL of your hosted server
+  host: BACKEND_URL, // Update this to the URL of your hosted server
   port: 10000, // Update this to the appropriate port if necessary
   logging: false // Optionally, set to false to disable logging SQL queries
 })
