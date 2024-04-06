ea8780d6806338cab8a530feea2c2a6008fe0b5f
abdrasaq14 <abdrasaq14@gmail.com>
2024-04-07 00:39:07 +0100

    fixed teh email sedning

--- src/utils/emailsender.ts
+++ src/utils/emailsender.ts
@@ -1,12 +1,14 @@
 import nodemailer from 'nodemailer'
-
+import { config } from 'dotenv'
+config()
+const { USER, APP_PASSWORD } = process.env
 export const transporter = nodemailer.createTransport({
   service: 'Gmail',
   host: 'smtp.gmail.com',
   port: 465,
   secure: true,
   auth: {
-    user: 'quickgradedecagon@gmail.com',
-    pass: 'tdynykegchtuzfog'
+    user: USER,
+    pass: APP_PASSWORD
   }
 })
